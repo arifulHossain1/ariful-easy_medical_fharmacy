5578bb00ab64b6b9e8939d35e52ad786aa5c4bd1
Md. Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-03-27 20:34:00 +0600

    Add files via upload

---
+++ HomeComponent.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class HomeComponent extends React.Component {
+    render() {
+        return (
+            <section className="content">
+                <div className="container-fluid">
+                    <div className="block-header">
+                        <h2>DASHBOARD</h2>
+                    </div>
+                    <div className="row clearfix">
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div className="info-box bg-pink hover-expand-effect">
+                                <div className="icon">
+                                    <i className="material-icons">playlist_add_check</i>
+                                </div>
+                                <div className="content">
+                                    <div className="text">HOME</div>
+                                    <div className="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20">125</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div className="info-box bg-cyan hover-expand-effect">
+                                <div className="icon">
+                                    <i className="material-icons">help</i>
+                                </div>
+                                <div className="content">
+                                    <div className="text">COMPANY</div>
+                                    <div className="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">257</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div className="info-box bg-light-green hover-expand-effect">
+                                <div className="icon">
+                                    <i className="material-icons">forum</i>
+                                </div>
+                                <div className="content">
+                                    <div className="text">MEDICINE</div>
+                                    <div className="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20">243</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div className="info-box bg-orange hover-expand-effect">
+                                <div className="icon">
+                                    <i className="material-icons">person_add</i>
+                                </div>
+                                <div className="content">
+                                    <div className="text">MANAGE MEDICINE</div>
+                                    <div className="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20">1225</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default HomeComponent;
