65e64f9d53ab8b1f276b4708dde152c3ae08b61b
Md. Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-04-11 00:01:42 +0600

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a href="#" className="bars" onClick={this.props.onBarClick}></a>
+                        <a className="navbar-brand" href="index.html"><b>EASY MEDICAL FHARMACY</b></a>
+                    </div>
+
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
